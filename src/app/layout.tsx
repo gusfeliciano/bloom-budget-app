bc995fcde4281c0362ca9a1596236e7e742c51db
gusfeliciano <gustav213@gmail.com>
2024-07-17 17:53:13 -0600

    added navigation sign in and sign up components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Sidebar from '../components/layout/Sidebar'
 import Header from '../components/layout/Header'
+import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function RootLayout({
   children,
@@ -10,16 +11,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex h-screen">
-          <Sidebar />
-          <div className="flex-1 flex flex-col">
-            <Header />
-            <main className="flex-1 p-6 bg-gray-100">
-              {children}
-            </main>
+        <AuthProvider>
+          <div className="flex h-screen">
+            <Sidebar />
+            <div className="flex-1 flex flex-col">
+              <Header />
+              <main className="flex-1 p-6 bg-gray-100">
+                {children}
+              </main>
+            </div>
           </div>
-        </div>
+        </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
